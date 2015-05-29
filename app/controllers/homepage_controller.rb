9c49cdc302ff3d80d0cbed595eaf3139c24ba5d2
Chirag Tailor <ctailor2@gmail.com>
2015-05-28 20:45:03 -0500

    Prep the controller for responding to js requests

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,12 @@
 class HomepageController < ApplicationController
+  respond_to :html, :json
+
   def index
+    respond_to do |format|
+      format.html {}
+      format.json do
+      end
+    end
   end
 end
 
