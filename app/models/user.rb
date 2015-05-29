11d3614fed08f95034dc32a312939c88cab3c208
Chirag Tailor <ctailor2@gmail.com>
2015-05-28 20:45:37 -0500

    Add user methods for retrieving pertinent fitbit client data

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,39 @@ class User < ActiveRecord::Base
   end
 
   def set_omniauth_credentials(auth)
-    oauth_token  = auth['credentials']['token']
-    oauth_secret = auth['credentials']['secret']
+    self.oauth_token  = auth['credentials']['token']
+    self.oauth_secret = auth['credentials']['secret']
+  end
+
+  def linked?
+    oauth_token.present? && oauth_secret.present?
+  end
+
+  def fitbit_client
+    raise 'Account is not linked with a Fitbit account' unless linked?
+    @fitbit_client ||= Fitgem::Client.new(
+      consumer_key: ENV['FITBIT_CONSUMER_KEY'],
+      consumer_secret: ENV['FITBIT_CONSUMER_SECRET'],
+      token: oauth_token,
+      secret: oauth_secret,
+      user_id: uid,
+      ssl: true
+    )
+  end
+
+  def info
+    @info ||= fitbit_client.user_info['user']
+  end
+
+  def full_name
+    info['fullName']
+  end
+
+  def timezone
+    info['timezone']
+  end
+
+  def step_count
+    fitbit_client.activities_on_date('today')['summary']['steps']
   end
 end
